2fa5ca25f2ba6bf850d464244a8be942a5a64fe0
Arpit Bhardwaj <arpit.bhardwaj@sap.com>
2020-08-29 18:48:56 +0530

    Consuming the orders call

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # JS Async Programming
 Demo code for java script promises
+
+In order to run the project
+
+npm install to install dev dependencies mentioned in package.json
+
+npm run dev to run the server
--- src/consuming.mjs
+++ src/consuming.mjs
@@ -1,15 +1,15 @@
-import setText, {appendText, showWaiting, hideWaiting} from './results.mjs';
+import setText, { appendText, showWaiting, hideWaiting } from './results.mjs';
 
-export function get(){
-}
-
-export function getCatch(){
-}
-
-export function chain(){
+export function get() {
+    axios.get("http://localhost:3000/orders/1")
+        .then(({ data }) => {
+            setText(JSON.stringify(data));
+        });
 }
 
-export function chainCatch(){
-}
-export function final(){
-}
+export function getCatch() {}
+
+export function chain() {}
+
+export function chainCatch() {}
+export function final() {}
